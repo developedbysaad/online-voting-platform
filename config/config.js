58a0dc65378fb36ca325d808e460e38ad095daaf
Mohd Saad <mail@mohdsaad.com>
2023-03-21 17:00:51 +0530

    Use environment variables

---
+++ config/config.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+module.exports = {
+  development: {
+    username: process.env.DEV_DB_USERNAME,
+    password: process.env.DEV_DB_PASSWORD,
+    database: process.env.DEV_DB_NAME,
+    host: process.env.DEV_DB_HOST,
+    dialect: "postgres",
+  },
+  test: {
+    username: process.env.TEST_DB_USERNAME,
+    password: process.env.TEST_DB_PASSWORD,
+    database: process.env.TEST_DB_NAME,
+    host: process.env.TEST_DB_HOST,
+    dialect: "postgres",
+  },
+  production: {
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOST,
+    dialect: "postgres",
+  },
+};
